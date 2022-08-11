9980bf6f25691afa8c1ce170dbd1f1eea5b503ec
ROHAN BHATIA <rohanb459@gmail.com>
2022-08-11 21:14:41 +0530

    cards added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let{title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>this is a news Item</div>
+      <div className="card my-3" style={{width: "18rem"}}>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsdetails/" className="btn btn-sm btn-primary">Read More</a>
+        </div>
+      </div>
     )
   }
 }
